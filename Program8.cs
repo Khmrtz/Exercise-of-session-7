7d9c3e79ce40ffe7fccaec2cfdbc019d74e6c9b3
Mortezazadeh <kh.mortezazade@gmail.com>
2024-11-03 15:33:50 +0330

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program8
+    {
+        static void Main()
+        {
+            int[,] Matrix1 = new int[2, 2];
+            int[,] Matrix2= new int[2, 2];
+            Console.WriteLine("Enter elements of first 2*2 matrix");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Element [{i},{j}]");
+                    Matrix1[i, j] = int.Parse(Console.ReadLine()??"");
+                }
+            }
+            Console.WriteLine("\nEnter elements of second 2*2 matrix");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Element [{i},{j}]");
+                    Matrix2[i, j] = int.Parse(Console.ReadLine() ?? "");
+                }
+            }
+            // Addition of Two matrices
+            int[,] SumResult = MatrixOperations.AddMatrix(Matrix1, Matrix2);
+            Console.WriteLine("\nSum of the matrices:");
+            for (int i = 0;i < 2; i++)
+            {
+                for(int j = 0;j < 2; j++)
+                {
+                    Console.WriteLine(SumResult[i, j]+"\t" );
+                }
+                Console.WriteLine();
+            }
+            // Multiplication of Two matrices
+            int[,] ProductResult = MatrixOperations.MultiplyMatrices(Matrix1, Matrix2);
+            Console.WriteLine("\nProduct of the matrices:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(ProductResult[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+    }
+}
---
+++ matris.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class MatrixOperations
+    { 
+        public static int[,] AddMatrix(int[,] Matrix1,int[,] Matrix2)
+        {
+            int[,] result=new int[2,2];
+            for(int i = 0;i<2;i++)
+            {
+                for(int j = 0;j<2;j++)
+                {
+                    result[i,j] = Matrix1[i, j] + Matrix2[i,j];
+                }
+            }
+            return result;
+        }
+        public static int[,] MultiplyMatrices(int[,] Matrix1, int[,] Matrix2)
+        {
+            int[,] result = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    result[i, j] = Matrix1[i, 0] * Matrix2[0, j]+ Matrix1[i, 1] * Matrix2[1, j];
+                }
+            }
+            return result;
+        }
+    }
+
+    
+}
+
+
